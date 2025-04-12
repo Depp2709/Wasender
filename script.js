ed0a5c9dfcebfe240a5074928f02d3b67cb1a9c9
Depp2709 <akmalmranggen@gmail.com>
2025-04-12 14:17:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ document.getElementById('whatsappForm').addEventListener('submit', function(even
     const message = document.getElementById('message').value;
     
     if (phoneNumber && message) {
+        // Format URL untuk WhatsApp
         const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+        // Buka URL di tab baru
         window.open(url, '_blank');
     } else {
         alert('Silakan isi nomor telepon dan pesan.');
